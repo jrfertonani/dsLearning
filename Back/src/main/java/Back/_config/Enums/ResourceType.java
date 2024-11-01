a79d5a623f31209a55d8b2e9a2d0a73b9587b2ec
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-01 10:17:14 -0300

    ResourceType

--- Back/src/main/java/Back/_config/Enums/ResourceType.java
+++ Back/src/main/java/Back/_config/Enums/ResourceType.java
@@ -3,33 +3,42 @@ package Back._config.Enums;
 import lombok.Getter;
 
 @Getter
-public enum Status {
+public enum ResourceType {
     // aqui são os enums
-    ANDAMENTO(0,"ANDAMENTO"),
-    ENCERRADO(1,"ENCERRADO");
+    LESSON_ONLY(0,"LESSON_ONLY"),
+    LESSON_TASK(1,"LESSON_TASK"),
+    FORUM(2,"FORUM"),
+    EXTERNAL_LINK(3,"EXTERNAL_LINK");
 
     //aqui as instancias
     private final Integer codigo;
     private final String descricao;
 
     //aqui construtor
-    Status(Integer codigo, String descricao) {
+    ResourceType(Integer codigo, String descricao) {
         this.codigo = codigo;
         this.descricao = descricao;
     }
 
     //aqui um codigo para verificar se existe ou não de enum
     //se existir retorna ele se não retorna uma exceção (não necessario)
-    public static Status toEnum(Integer cod){
+    public static ResourceType toEnum(Integer cod){
         if( cod == null){
             return null;
         }
-        for(Status x : Status.values()){
+        for(ResourceType x : ResourceType.values()){
             if(cod.equals(x.getCodigo())){
                 return x;
             }
         }
-        throw new IllegalArgumentException("Status inválido: " + cod);
+        throw new IllegalArgumentException("ResourceType inválido: " + cod);
     }
 
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 }
